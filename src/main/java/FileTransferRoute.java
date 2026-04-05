775d0959dc1618d33ba6e592e9a1450f350d561d
Mattair39 <105869714+Mattair39@users.noreply.github.com>
2026-04-05 18:43:23 -0500

    Adición de carpeta Archived y Logs de Transferencia de Archivos

--- src/main/java/FileTransferRoute.java
+++ src/main/java/FileTransferRoute.java
@@ -11,9 +11,19 @@ public class FileTransferRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+
         from("file:d:/Cursos/IntegracionSistemas/camel-lab/input?noop=true")
+            .filter(header("CamelFileName").endsWith(".csv"))
             .log("Procesando archivo: ${file:name}")
+            .wireTap("direct:registrarLog")
+            .convertBodyTo(String.class)
             .transform().simple("${body.toUpperCase()}")
-            .to("file:d:/Cursos/IntegracionSistemas/camel-lab/output");
+            .to("file:d:/Cursos/IntegracionSistemas/camel-lab/output")
+            .to("file:d:/Cursos/IntegracionSistemas/camel-lab/archived");
+
+        from("direct:registrarLog")
+            .transform().simple("${date:now:yyyy-MM-dd HH:mm:ss} | Procesado: ${header.CamelFileName}\n")
+            .to("file:d:/Cursos/IntegracionSistemas/camel-lab/logs"
+                + "?fileName=transfers-${date:now:yyyy-MM-dd-HH'h'mm}.log&fileExist=Append&charset=UTF-8");
     }
 }
