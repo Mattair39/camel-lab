7a45623343c50e7805d9f1cc167149ed0013ef56
Mattair39 <105869714+Mattair39@users.noreply.github.com>
2026-04-08 16:49:58 -0500

    Adición filtro y .gitignore

--- src/main/java/FileTransferRoute.java
+++ src/main/java/FileTransferRoute.java
@@ -13,17 +13,23 @@ public class FileTransferRoute extends RouteBuilder {
     public void configure() throws Exception {
 
         from("file:d:/Cursos/IntegracionSistemas/camel-lab/input?noop=true")
-            .filter(header("CamelFileName").endsWith(".csv"))
-            .log("Procesando archivo: ${file:name}")
-            .wireTap("direct:registrarLog")
-            .convertBodyTo(String.class)
-            .transform().simple("${body.toUpperCase()}")
-            .to("file:d:/Cursos/IntegracionSistemas/camel-lab/output")
-            .to("file:d:/Cursos/IntegracionSistemas/camel-lab/archived");
+                .filter(header("CamelFileName").endsWith(".csv"))
+                .log("Procesando Archivo: ${file:name}")
+                .wireTap("direct:registrarLog")
+                .convertBodyTo(String.class)
+                .transform().simple("${body.toUpperCase()}")
+
+                .to("file:d:/Cursos/IntegracionSistemas/camel-lab/output")
+                .log("Archivo enviado a output: ${file:name} - ${date:now:yyyy-MM-dd HH:mm:ss}")
+
+                .filter(simple("${file:name} != 'inventario.csv'"))
+                .to("file:d:/Cursos/IntegracionSistemas/camel-lab/archived")
+                .log("Archivo enviado a archived: ${file:name} - ${date:now:yyyy-MM-dd HH:mm:ss}")
+                .end();
 
         from("direct:registrarLog")
-            .transform().simple("${date:now:yyyy-MM-dd HH:mm:ss} | Procesado: ${header.CamelFileName}\n")
-            .to("file:d:/Cursos/IntegracionSistemas/camel-lab/logs"
-                + "?fileName=transfers-${date:now:yyyy-MM-dd-HH'h'mm}.log&fileExist=Append&charset=UTF-8");
+                .transform().simple("${date:now:yyyy-MM-dd HH:mm:ss} | Procesando Archivo: ${header.CamelFileName}${sys.line.separator}")
+                .to("file:d:/Cursos/IntegracionSistemas/camel-lab/logs"
+                        + "?fileName=transfers-${date:now:yyyy-MM-dd-HH'h'mm}.log&fileExist=Append&charset=UTF-8");
     }
 }
